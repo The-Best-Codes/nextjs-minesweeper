d9e990c823e0aec6e148be5b3d0f40ad41a05fc6
Best Codes <106822363+The-Best-Codes@users.noreply.github.com>
2025-01-17 16:45:16 +0000

    testpwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// @ts-ignore
+const nextConfig: NextConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})({
+  /* your existing config options here */
+});
 
 export default nextConfig;
